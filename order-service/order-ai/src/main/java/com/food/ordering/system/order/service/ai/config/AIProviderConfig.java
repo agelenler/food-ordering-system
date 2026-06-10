7a22ea1bfa0e562ccc99ce5d71f1dd18b6c5bb47
ali gelenler <you@example.com>
2026-06-10 22:06:16 +0200

    Added ollama to run local llms

--- order-service/order-ai/src/main/java/com/food/ordering/system/order/service/ai/config/AIProviderConfig.java
+++ order-service/order-ai/src/main/java/com/food/ordering/system/order/service/ai/config/AIProviderConfig.java
@@ -1,15 +1,32 @@
 package com.food.ordering.system.order.service.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AIProviderConfig {
 
-    @Bean("openAIChatClient")
-    ChatClient openAIChatClient(OpenAiChatModel openAiChatModel) {
-        return ChatClient.builder(openAiChatModel).build();
+    @Bean("orderNoteInterpreterChatClient")
+    @ConditionalOnProperty(prefix = "order.ai", name = "provider", havingValue = "openai", matchIfMissing = true)
+    ChatClient openAIChatClient(OpenAiChatModel openAiChatModel, SimpleLoggerAdvisor simpleLoggerAdvisor) {
+        return ChatClient.builder(openAiChatModel).defaultAdvisors(simpleLoggerAdvisor).build();
     }
+
+    @Bean("orderNoteInterpreterChatClient")
+    @ConditionalOnProperty(prefix = "order.ai", name = "provider", havingValue = "ollama")
+    ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel, SimpleLoggerAdvisor simpleLoggerAdvisor) {
+        return ChatClient.builder(ollamaChatModel).defaultAdvisors(simpleLoggerAdvisor).build();
+    }
+
+    @Bean
+    SimpleLoggerAdvisor simpleLoggerAdvisor() {
+        return new SimpleLoggerAdvisor();
+    }
+
+
 }
--- order-service/order-ai/src/main/java/com/food/ordering/system/order/service/ai/interpreter/adapter/AIOrderNoteInterpreter.java
+++ order-service/order-ai/src/main/java/com/food/ordering/system/order/service/ai/interpreter/adapter/AIOrderNoteInterpreter.java
@@ -87,7 +87,7 @@ public class AIOrderNoteInterpreter implements OrderNoteInterpreter {
     private final Resource orderNoteInterpreterPrompt;
     private final int maxAttempts;
 
-    public AIOrderNoteInterpreter(@Qualifier("openAIChatClient") ChatClient chatClient,
+    public AIOrderNoteInterpreter(@Qualifier("orderNoteInterpreterChatClient") ChatClient chatClient,
                                   @Value("classpath:/templates/order-interpreter-prompt.st") Resource orderNoteInterpreterPrompt,
                                   @Value("${order.ai.interpreter.max-attempts:3}") int maxAttempts) {
         this.chatClient = chatClient;
